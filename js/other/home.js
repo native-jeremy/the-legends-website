2e3e05984d1a63bbadaf7e1039f268e737c38976
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-11-17 15:18:34 +1100

    Update home.js

--- js/other/home.js
+++ js/other/home.js
@@ -1,16 +1,16 @@
+anime({
+  targets: '.path3',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'cubicBezier(.5, .05, .1, .3)',
+  duration: 2000,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
 //----------------------------------------------------------------
 //  HOME PAGE REQUEST | FROM WIZED
 //----------------------------------------------------------------
 Wized.request.await("Load Home Page", (response) => {
-  anime({
-    targets: '.path3',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'cubicBezier(.5, .05, .1, .3)',
-    duration: 2000,
-    delay: function(el, i) { return i * 250 },
-    direction: 'alternate',
-    loop: true
-  });
   const snapshot = response.data;
   //console.log(snapshot);
 //----------------------------------------------------------------
