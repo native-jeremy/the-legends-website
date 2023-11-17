49e986585907259aefdc693efbacd1dc00aa3fdf
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-11-17 14:20:57 +1100

    Update

--- js/other/home.js
+++ js/other/home.js
@@ -66,6 +66,18 @@ Wized.request.await("Load Home Page", (response) => {
       }
       },
       mounted(){
+        const userId = Wized.data.get('c.userid');
+        console.log('UserID: ', userId);
+        const mainBtn = document.getElementById('mainBtn');
+    
+        if(userId !== undefined || userId !== null)
+        {
+            mainBtn.href = "/program-hub-welcome.html";
+            mainBtn.textContent = "Home";
+        }
+        else {
+            mainBtn.textContent = "Login";
+        }
       function delayedSelection() {
           console.log("interaction loaded");
           window.Webflow && window.Webflow.destroy();
