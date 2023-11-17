9caf97ac537028545f849a7f9973147e3acc7486
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-11-17 14:15:02 +1100

    Update userid.js

--- js/other/userid.js
+++ js/other/userid.js
@@ -1,14 +1,14 @@
 window.onload = async () => {
-    const userId = localStorage.getItem('recpies_stored');
+    const userId = JSON.parse(localStorage.getItem('recpies_stored'));
     console.log('UserID: ', userId);
     const mainBtn = document.getElementById('mainBtn');
 
-    if(!userId)
+    if(userId !== true)
     {
         mainBtn.href = "/program-hub-welcome.html";
         mainBtn.textContent = "Home";
     }
-    else if (userId) {
+    else if (userId == true) {
         mainBtn.textContent = "Login";
     }
   };
