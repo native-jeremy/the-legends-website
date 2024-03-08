b60bfb66f8f8e2e8bb6b28d87b55d4bd80bc2af5
Morgan Brown <mjxbrown@gmail.com>
2024-03-09 02:21:15 +1100

    Update

--- js/programs/program-selection.js
+++ js/programs/program-selection.js
@@ -9,37 +9,8 @@ window.onload = async () => {
     loop: true
 });
 
-    // New Stripe Setup
-    let stripe = Stripe(
-      "pk_test_51MbG9BBV1W2mjCG5QN8s7AylZVu9IRRjtnXMWz3XpCUqYCgcz2J6BOEMvHUTapTJWmv3ApodZObxopkXm3RW9UKl00aBDTKIgK"
-    );
-    let checkoutButton = document.getElementById("stripe");
 
-    checkoutButton.addEventListener("click", function () {
-      // Create a new Checkout Session using the server-side endpoint you
-      // created in step 3.
-      fetch("/api/stripe", {
-        method: "POST",
-        body: "morganbrown756@gmail.com"
-      })
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (session) {
-          return stripe.redirectToCheckout({ sessionId: session.id });
-        })
-        .then(function (result) {
-          // If `redirectToCheckout` fails due to a browser or network
-          // error, you should display the localized error message to your
-          // customer using `error.message`.
-          if (result.error) {
-            alert(result.error.message);
-          }
-        })
-        .catch(function (error) {
-          console.error("Error:", error);
-        });
-    });
+    //let checkoutButton = document.getElementById("stripe");
 
     //Element Variables
     const sliderControls = document.querySelector(".slider-controls");
@@ -81,6 +52,39 @@ window.onload = async () => {
           errorModal.style.display = "flex";
           //console.log("Selected!")
       }
+      
+    // New Stripe Setup
+    let stripe = Stripe(
+      "pk_test_51MbG9BBV1W2mjCG5QN8s7AylZVu9IRRjtnXMWz3XpCUqYCgcz2J6BOEMvHUTapTJWmv3ApodZObxopkXm3RW9UKl00aBDTKIgK"
+    );
+    let checkoutButton = document.getElementById("stripe");
+        console.log("EMAIL: ", user.Email)
+        checkoutButton.addEventListener("click", function () {
+          // Create a new Checkout Session using the server-side endpoint you
+          // created in step 3.
+          fetch("/api/stripe", {
+            method: "POST",
+            body: `${user.Email}`
+          })
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (session) {
+              return stripe.redirectToCheckout({ sessionId: session.id });
+            })
+            .then(function (result) {
+              // If `redirectToCheckout` fails due to a browser or network
+              // error, you should display the localized error message to your
+              // customer using `error.message`.
+              if (result.error) {
+                alert(result.error.message);
+              }
+            })
+            .catch(function (error) {
+              console.error("Error:", error);
+            });
+        });
+      
   
       Wized.request.await("Load Program Selection", (response) => {
         programData = response.data;
--- netlify/functions/stripe.js
+++ netlify/functions/stripe.js
@@ -3,7 +3,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 exports.handler = async (event, context) => {
   const email = event.body;
-  console.log('Email: ',email);
+  //console.log('Email: ',email);
 
   // Stripe Checkout Session | V2 Server-Client Integration
   const session = await stripe.checkout.sessions.create({
@@ -17,8 +17,8 @@ exports.handler = async (event, context) => {
       trial_period_days: 14,
     },
     customer_email: `${email}`,
-    success_url: `http://localhost:8888/success.html?success&session_id={CHECKOUT_SESSION_ID}&userverfied=${email}`,
-    cancel_url: "http://localhost:8888/",
+    success_url: `https://thelegendsprogram.com.au/thank-you.html?success&session_id={CHECKOUT_SESSION_ID}&userverified=${email}`,
+    cancel_url: `https://thelegendsprogram.com.au/program-selection.html"`,
   });
 
   return {
