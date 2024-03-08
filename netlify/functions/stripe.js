c3d1989e7b2daa10d5fe6cd0f8b5fb156fb93d48
Morgan Brown <mjxbrown@gmail.com>
2024-03-09 02:11:21 +1100

    Update

--- netlify/functions/stripe.js
+++ netlify/functions/stripe.js
@@ -18,7 +18,7 @@ exports.handler = async (event, context) => {
     },
     //customer_email: `${email}`,
     success_url: `thelegendsprogram.com.au/thank-you.html?success&session_id={CHECKOUT_SESSION_ID}&userverified=${email}`,
-    cancel_url: `thelegendsprogram.com.au/program-selection.html"`,
+    cancel_url: `thelegendsprogram.com.au/program-selection.html`,
   });
 
   return {
