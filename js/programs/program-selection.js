2e40efc345728f2db275d876489bfff303ee5d29
Morgan Brown <mjxbrown@gmail.com>
2024-03-09 03:14:00 +1100

    Update

--- js/programs/program-selection.js
+++ js/programs/program-selection.js
@@ -78,8 +78,27 @@ window.onload = async () => {
     let yearlyButton = document.getElementById("stripeYearly");
 
     monthlyButton.addEventListener("click", function () {
-      // Create a new Checkout Session using the server-side endpoint you
-      stripePost(monthly)
+      fetch("/api/stripe", {
+        method: "POST",
+        //body: stripeBody
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (session) {
+          return stripe.redirectToCheckout({ sessionId: session.id });
+        })
+        .then(function (result) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, you should display the localized error message to your
+          // customer using `error.message`.
+          if (result.error) {
+            alert(result.error.message);
+          }
+        })
+        .catch(function (error) {
+          console.error("Error:", error);
+        });
     });
 
     quarterlyButton.addEventListener("click", function () {
