b8f82597876f67b4d2939062964ab087468d6b27
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2024-06-05 09:58:50 +1000

    Updates

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,22 @@
 module.exports = {
-	globDirectory: './',
-	globPatterns: [
-		'**/*.{html,css,ico,jpg,svg,png,js}'
-	],
-	swDest: 'sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+    globDirectory: './',
+    globPatterns: [
+        '**/*.{html,css,ico,jpg,svg,png,js}'
+    ],
+    swDest: 'sw.js',
+    ignoreURLParametersMatching: [
+        /^utm_/,
+        /^fbclid$/
+    ],
+    runtimeCaching: [
+        {
+            urlPattern: ({request}) => request.destination === 'script',
+            handler: 'StaleWhileRevalidate',
+            options: {
+                cacheName: 'javascript-cache',
+            },
+        },
+    ],
+    clientsClaim: true,
+    skipWaiting: true,
 };
