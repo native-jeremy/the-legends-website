7b68176c42224c5ca0b23ed0311baefafdaec350
Morgan Brown <mjxbrown@gmail.com>
2025-05-28 13:39:43 +1000

    Update guides.js

--- js/guides/guides.js
+++ js/guides/guides.js
@@ -1,9 +1,15 @@
 window.onload = async () => {
+  anime({
+    targets: '.path3',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'cubicBezier(.5, .05, .1, .3)',
+    duration: 2000,
+    delay: function(el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
 
-
-    await Wized.request.execute('Load Guide');
-    const response = await Wized.data.get('r.40.d');
-    console.log("Data: ", response)
+  Wized.request.await("Load Guide", (response) => {
     setTimeout(SlideInfo, 2000);
     function SlideInfo() {
       const snapshot = response.data;
@@ -52,4 +58,5 @@ window.onload = async () => {
       }
     }
     document.querySelector('.loading-state-v2').style.display = "none"
+  });
 };
