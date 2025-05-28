a3e8fe87179fd4c563e134a6904486772f7925bd
Morgan Brown <mjxbrown@gmail.com>
2025-05-28 12:48:51 +1000

    Update guides.js

--- js/guides/guides.js
+++ js/guides/guides.js
@@ -8,8 +8,60 @@ window.onload = async () => {
     direction: 'alternate',
     loop: true
   });
+  await Wized.request.execute('Load Guide'); // Trigger request
+  const response = await Wized.data.get('r.40.d'); // Get request response
+  console.log(response); // Console log received request data
 
-  Wized.request.await("Load Guide", (response) => {
+      setTimeout(SlideInfo, 2000);
+      function SlideInfo() {
+        const snapshot = response.data;
+        const titles = snapshot.Slides_Titles;
+        const descs = snapshot.Slides_Descriptions;
+        const completeButton = document.querySelector(".complete_guide");
+        let titlesCol = titles.split("," + "\n");
+        let descsCol = descs.split("," + "\n");
+        document.title = snapshot.Name;
+        const swiper = new Swiper(".guide-slider", {
+          speed: 1000,
+          loop: false,
+          observer: true,
+          autoHeight: true,
+          observeParents: true,
+          slidesPerView: 1,
+          shortSwipes: true,
+          watchSlidesProgress: true,
+          initialSlide: 0,
+          navigation: {
+            nextEl: ".right-slide-arrow-button",
+            prevEl: ".left-slide-arrow-button",
+          },
+        });
+        swiper.on("reachEnd", function () {
+          completeButton.style.display = "flex";
+        });
+        swiper.on("slideChange", function (e) {
+          if (swiper.activeIndex == swiper.slides.length - 1) {
+            completeButton.style.display = "flex";
+          } else {
+            completeButton.style.display = "none";
+          }
+        });
+        completeButton.addEventListener("click", () => {
+          setTimeout(() => {
+            history.back();
+          }, 3000);
+        });
+        let slide = document.querySelectorAll(".swiper-slide");
+        let heading = document.querySelectorAll(".slide-heading");
+        let bodyCopy = document.querySelectorAll(".slide-body-copy");
+        for (let i = 0; i < slide.length; i++) {
+          heading[i].innerHTML = titlesCol[i];
+          bodyCopy[i].innerHTML = descsCol[i];
+        }
+        document.querySelector('.loading-state-v2').style.display = "none"
+      }
+
+  /*Wized.request.await("Load Guide", (response) => {
     setTimeout(SlideInfo, 2000);
     function SlideInfo() {
       const snapshot = response.data;
@@ -56,7 +108,7 @@ window.onload = async () => {
         heading[i].innerHTML = titlesCol[i];
         bodyCopy[i].innerHTML = descsCol[i];
       }
-    }
+    }*
     document.querySelector('.loading-state-v2').style.display = "none"
-  });
+  });*/
 };
