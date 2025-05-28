b4a5a8ee49a0b2a6d9f519af34a6a909ba66d9d8
Morgan Brown <mjxbrown@gmail.com>
2025-05-28 13:37:59 +1000

    Update guides.js

--- js/guides/guides.js
+++ js/guides/guides.js
@@ -1,13 +1,5 @@
 window.onload = async () => {
-  anime({
-    targets: '.path3',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'cubicBezier(.5, .05, .1, .3)',
-    duration: 2000,
-    delay: function(el, i) { return i * 250 },
-    direction: 'alternate',
-    loop: true
-  });
+
 
     await Wized.request.execute('Load Guide');
     const response = await Wized.data.get('r.40.d');
