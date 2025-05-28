6920410b51ba0a125259f57d2e79c4e064121824
Morgan Brown <mjxbrown@gmail.com>
2025-05-28 18:35:15 +1000

    Updates

--- js/guides/guides.js
+++ js/guides/guides.js
@@ -1,78 +1,15 @@
-window.onload = async () => {
-  anime({
-    targets: '.path3',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'cubicBezier(.5, .05, .1, .3)',
-    duration: 2000,
-    delay: function(el, i) { return i * 250 },
-    direction: 'alternate',
-    loop: true
-  });
-
-  /*Wized.request.await("Load Guide", (response) => {
-    setTimeout(SlideInfo, 2000);
-    function SlideInfo() {
-      const snapshot = response.data;
-      const titles = snapshot.Slides_Titles;
-      const descs = snapshot.Slides_Descriptions;
-      const completeButton = document.querySelector(".complete_guide");
-      let titlesCol = titles.split("," + "\n");
-      let descsCol = descs.split("," + "\n");
-      document.title = snapshot.Name;
-      const swiper = new Swiper(".guide-slider", {
-        speed: 1000,
-        loop: false,
-        observer: true,
-        autoHeight: true,
-        observeParents: true,
-        slidesPerView: 1,
-        shortSwipes: true,
-        watchSlidesProgress: true,
-        initialSlide: 0,
-        navigation: {
-          nextEl: ".right-slide-arrow-button",
-          prevEl: ".left-slide-arrow-button",
-        },
-      });
-      swiper.on("reachEnd", function () {
-        completeButton.style.display = "flex";
-      });
-      swiper.on("slideChange", function (e) {
-        if (swiper.activeIndex == swiper.slides.length - 1) {
-          completeButton.style.display = "flex";
-        } else {
-          completeButton.style.display = "none";
-        }
-      });
-      completeButton.addEventListener("click", () => {
-        setTimeout(() => {
-          history.back();
-        }, 3000);
-      });
-      let slide = document.querySelectorAll(".swiper-slide");
-      let heading = document.querySelectorAll(".slide-heading");
-      let bodyCopy = document.querySelectorAll(".slide-body-copy");
-      for (let i = 0; i < slide.length; i++) {
-        heading[i].innerHTML = titlesCol[i];
-        bodyCopy[i].innerHTML = descsCol[i];
-      }
-    }
-    document.querySelector('.loading-state-v2').style.display = "none"
-  });*/
-
-    // Get the query string from the URL
-  const queryString = window.location.search;
-
-  // Parse the query string
-  const params = new URLSearchParams(queryString);
-
-  // Get the 'recipe' parameter
-  const ID = params.get("guide");
-  console.log("ID:", ID);
-
-  fetchCollectionItem("Guides", collectionItem);
-
-  async function fetchCollectionItem(collectionName, ID) {
+// Start Vue Intializer
+const { createApp } = Vue;
+createApp({
+  data() {
+    return {
+      response: null,
+      slides: [],
+      completed: false
+    };
+  },
+  methods: {
+  async fetchCollectionItem(collectionName, ID) {
         fetch(`/api/collectionItem?collection=${encodeURIComponent(collectionName)}&recordId=${encodeURIComponent(ID)}`, {
       method: "GET",
       headers: {
@@ -86,15 +23,35 @@ window.onload = async () => {
         return response.json();
       })
       .then((data) => {
-      setTimeout(SlideInfo(data), 2000);
-      function SlideInfo(response) {
-        const snapshot = response.data;
-        const titles = snapshot.Slides_Titles;
-        const descs = snapshot.Slides_Descriptions;
-        const completeButton = document.querySelector(".complete_guide");
-        let titlesCol = titles.split("," + "\n");
-        let descsCol = descs.split("," + "\n");
-        document.title = snapshot.Name;
+        console.log(data)
+        this.response = data;
+        document.title = data.Name;
+        this.generateSlides();
+        //setTimeout(this.SlideSetup(data), 2000);
+      document.querySelector('.loading-state-v2').style.display = "none"
+      });
+  },
+  generateSlides() {
+    const titles = this.formatStringToArray(',', this.response.Slides_Titles);
+    const descriptions = this.formatStringToArray(',', this.response.Slides_Descriptions);
+      for (let i = 0; i < this.response.Slides.length; i++) {
+        const slide = {
+          image: this.response.Slides_Images[i].url,
+          title: titles[i],
+          description: descriptions[i]
+        }
+        this.slides.push(slide);
+      }
+     setTimeout(this.slideSetup(), 2000);   
+  },
+  formatStringToArray (seperator, data) {
+      let Array = [];
+      if(data) {
+        Array = data.split(`${seperator}` + "\n");
+      }
+      return Array;
+  },
+  slideSetup() {
         const swiper = new Swiper(".guide-slider", {
           speed: 1000,
           loop: false,
@@ -111,29 +68,48 @@ window.onload = async () => {
           },
         });
         swiper.on("reachEnd", function () {
-          completeButton.style.display = "flex";
+          this.completed = true;
         });
         swiper.on("slideChange", function (e) {
           if (swiper.activeIndex == swiper.slides.length - 1) {
-            completeButton.style.display = "flex";
+            this.completed = true;
           } else {
-            completeButton.style.display = "none";
+            this.completed = false;
           }
         });
-        completeButton.addEventListener("click", () => {
-          setTimeout(() => {
-            history.back();
-          }, 3000);
-        });
-        let slide = document.querySelectorAll(".swiper-slide");
-        let heading = document.querySelectorAll(".slide-heading");
-        let bodyCopy = document.querySelectorAll(".slide-body-copy");
-        for (let i = 0; i < slide.length; i++) {
-          heading[i].innerHTML = titlesCol[i];
-          bodyCopy[i].innerHTML = descsCol[i];
-        }
-      }
-      document.querySelector('.loading-state-v2').style.display = "none"
-      });
+  },
+  async readGuide() {
+    console.log("Data")
+    await Wized.request.execute('Read Guide');
+    const response = await Wized.data.get('r.128.d'); // Get request response
+    if(response) {
+      setTimeout(() => {
+        history.back();
+      }, 3000);
+    }
   }
-};
+  },
+  mounted() {
+
+    // Get the query string from the URL
+    const queryString = window.location.search;
+
+    // Parse the query string
+    const params = new URLSearchParams(queryString);
+
+    // Get the 'guide' parameter
+    const ID = params.get("guide");
+    console.log("ID:", ID);
+    this.fetchCollectionItem("Guides", ID);
+
+    setTimeout(() => {
+      console.log("Slides: ", this.slides)
+    console.log("interaction loaded");
+    window.Webflow && window.Webflow.destroy();
+    window.Webflow && window.Webflow.ready();
+    window.Webflow && window.Webflow.require("ix2").init();
+    document.dispatchEvent(new Event("readystatechange"));
+    }, 3000);
+  },
+}).mount("#app");
+// End Vue Intializer
